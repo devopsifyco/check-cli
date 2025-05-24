0a423aa9e9ac825259975a7df611a31e56ffc697
Huy <huyntt@gmail.com>
2025-05-24 15:45:58 +0700

    Update docs to support 'Edit on GitHub' links using html_context

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,4 @@
+# Updated to support 'Edit on GitHub' links using html_context
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
@@ -12,4 +13,13 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static'] 
+html_static_path = ['_static']
+
+# Update the following html_context values with your actual GitHub username and repository name
+html_context = {
+    "display_github": True,  # Integrate GitHub
+    "github_user": "devopsifyco",  # GitHub username or org
+    "github_repo": "check-cli",  # Repository name
+    "github_version": "master",  # Branch or tag
+    "doc_path": "docs",  # Path to your docs root in the repo
+} 
